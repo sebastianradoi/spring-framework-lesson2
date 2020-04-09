4fffde431366511d08dac8328d836277cc7335c2
sebastian_radoi <sebastian.radoi@enghouse.com>
2020-04-09 21:13:26 +0300

    first commit

---
+++ src/main/java/beans/Cat.java
@@ -0,0 +1,18 @@
+package beans;
+
+public class Cat {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Cat: " + this.name;
+	}
+}
---
+++ src/main/java/beans/Owner.java
@@ -0,0 +1,22 @@
+package beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Owner {
+
+	@Autowired
+	private Cat cat;
+
+	public Cat getCat() {
+		return cat;
+	}
+
+	public void setCat(Cat cat) {
+		this.cat = cat;
+	}
+
+	@Override
+	public String toString() {
+		return "Owner: {Cat: "+this.getCat().getName()+"}";
+	}
+}
